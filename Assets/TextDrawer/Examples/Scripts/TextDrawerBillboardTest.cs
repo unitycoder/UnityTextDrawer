94f75f77cade586fd5536cfc9fd84c424b68209c
mika <support@unitycoder.com>
2024-09-03 10:56:55 +0300

    add more interesting billboard demo scene

--- Assets/TextDrawer/Examples/Scripts/TextDrawerBillboardTest.cs
+++ Assets/TextDrawer/Examples/Scripts/TextDrawerBillboardTest.cs
@@ -6,8 +6,24 @@ public class TextDrawerBillboardTest : MonoBehaviour
 {
     public TextDrawer.TextPivot Pivot;
 
+    Vector3[] positions = new Vector3[100];
+    Color[] colors = new Color[100];
+
+    private void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            positions[i] = transform.position + Random.insideUnitSphere * 25f;
+            colors[i] = Random.ColorHSV();
+        }
+
+    }
+
     private void LateUpdate()
     {
-        TextDrawer.DrawTextFacingCamera("Billboard", 12f, Color.red, transform.position);
+        for (int i = 0; i < 100; i++)
+        {
+            TextDrawer.DrawTextFacingCamera("Billboard" + i, 12f, colors[i], positions[i]);
+        }
     }
 }
